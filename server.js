af6e14becc13d26f2684bfd03ba1977784b178cf
voisnoa <markzlopp@gmail.com>
2025-03-19 10:09:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require("mongoose")
+const model = require("./schema")
+
+dotenv.config()
+const app = express()
+
+PORT = 8808
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(
+        console.log('MONGO is running')
+    )
+
+app.post("/login" , (req,res) =>{
+
+    try{
+        const {email , password} = req.body
+    
+        if (!email){
+            res.send(() =>{
+                "Email cannot be empty."
+            });
+        
+        }
+        if(!password){
+            res.send(() =>{
+                "Password cannot be empty."
+            })
+        }
+
+        res.status(201).json({message : "User is successfully logged in"})
+    }
+    catch{
+        res.status(404).json({message: "Wrong email or password"})
+    }
+})
+
+
